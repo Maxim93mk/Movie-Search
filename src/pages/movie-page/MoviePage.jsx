f4f5b6616d12462d172431d616c9b7b1423c628c
Maxim93mk <kinjashov.m@rambler.ru>
2024-09-04 22:54:18 +0300

    Добавлены переходы между страницами

--- src/pages/movie-page/MoviePage.jsx
+++ src/pages/movie-page/MoviePage.jsx
@@ -31,8 +31,9 @@ function MoviePage() {
     }
     else {
         return (
+            
             <>
-                <Header />
+                <Header fetchData={()=>fetchData()} />
                 <main>
 
                     <div className="wrapper">
