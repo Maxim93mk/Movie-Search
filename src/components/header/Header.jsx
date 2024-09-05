8d3f29b40d4c658013be3bfee41f2dbaf99b0eb4
Maxim93mk <kinjashov.m@rambler.ru>
2024-09-05 18:21:29 +0300

    Исправлено формирование строки для запроса

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,40 +1,32 @@
 import React, { useState, useEffect } from "react";
 import styles from './Header.module.css';
-import { useNavigate } from "react-router-dom";
 
 function Header(props) {
     const [stringSearch, setStringSearch] = useState(''); // Значение поисковой строки
-    
     const param = '?s=';
-    let query = '';
-    // const navigate = useNavigate();
 
     // Загрузка страницы
     useEffect(() => {
         props.fetchData();
     }, []);
 
-    // Проверка нажатия клавиши Enter
-    const handleEnter = (evt) => {
-        if (evt.key === 'Enter') {
-            console.log("lklkdk")
-            props.fetchData(query);
-            props.setFlagComponent(false);
-            // redirectMainPage();
+
+    // Формирование строки для запроса
+    const setSearchQuery = (str) => {
+        if (str !== '') {
+            setStringSearch(param + str)
+        }
+        else {
+            setStringSearch(str);
         }
     }
 
-    // const redirectMainPage = () => {
-    //     return navigate('/');
-    // }
+    // Отправка на запрос фильма из поисковой строки
+    const sendSearchQuery = () => {
+        props.fetchData(stringSearch);
+        props.setFlagComponent(false);
+    }
 
-    if(stringSearch===''){
-        query = stringSearch;
-    }
-    else{
-        query = param + stringSearch;
-    }
-    console.log(query,stringSearch)
     return (
         <>
             <header>
@@ -45,11 +37,11 @@ function Header(props) {
                             <input type="text"
                                 className={styles.searchInput}
                                 placeholder='Введите название фильма...'
-                                onChange={(evt) => setStringSearch(evt.target.value)}
-                                onKeyUp={(evt) => (handleEnter(evt))}
+                                onChange={(evt) => setSearchQuery(evt.target.value)}
+                                onKeyUp={(evt) => evt.key === 'Enter' ? sendSearchQuery() : ''}
                             />
                             <button className={styles.searchBtn}
-                                onClick={() => props.fetchData(query)}
+                                onClick={() => sendSearchQuery()}
                             ></button>
                         </div>
                     </section>
--- src/pages/main-page/MainPage.jsx
+++ src/pages/main-page/MainPage.jsx
@@ -8,26 +8,29 @@ import MovieDescr from '../../components/movie-descr/MovieDescr';
 function MainPage() {
     const { movies, error, fetchData } = useGetMovies();
     const [getFlagComponent, setFlagComponent] = useState(false);
-    console.log(getFlagComponent);
-
-    // if(getFlagComponent){
-    //     setComponent(<CardList movies={movies} error={error} />);
-    // }
-    // else{
-
-    // }
-    const component = !getFlagComponent ? <CardList movies={movies} error={error} setFlagComponent={setFlagComponent} /> : <MovieDescr />;
+    
+    
+    const getComponent = () =>{
+        if(!getFlagComponent){
+            return <CardList movies={movies} error={error} setFlagComponent={setFlagComponent} />;
+        }
+        else{
+            return <MovieDescr />;
+        }
+    }
+    
+    
+    // const component = !getFlagComponent ?
+        
+    //     :
+        
 
     return (
         <>
             <Header fetchData={fetchData}
                 setFlagComponent={setFlagComponent} />
             <main>
-                <div className="wrapper">
-                    {component}
-
-                    {/* <MoviePage /> */}
-                </div>
+                <div className="wrapper">{getComponent()}</div>
             </main>
         </>
     );
