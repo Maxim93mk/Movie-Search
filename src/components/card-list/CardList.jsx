f4744f31850b732e975c59eec17acdddce0de72b
Maxim93mk <kinjashov.m@rambler.ru>
2024-09-05 20:14:14 +0300

    Перенесены обработчики ошибок в MainPage.jsx

--- src/components/card-list/CardList.jsx
+++ src/components/card-list/CardList.jsx
@@ -4,52 +4,25 @@ import NotFound from "../not-found/NotFound";
 import styles from './CardList.module.css';
 
 function CardList(props) {
-    const setFlagComponent=props.setFlagComponent;
-    const setIdMovie=props.setIdMovie;
-    
-    if (props.error) {
-        return (
-            <>
-                <section className={styles.NotFound}>
-                    <NotFound TitleMsg={'404'}
-                        DescrMsg={'Not Found'} />
-                </section>
-            </>
-        )
+    const setFlagComponent = props.setFlagComponent;
+    const setIdMovie = props.setIdMovie;
 
-    }
-    else {
-         if (props.movies.length===0 || props.movies.Response==='False') {
-            return (
-                <>
-                    <section className={styles.NotFound}>
-                        <NotFound TitleMsg={'Oops...'}
-                            DescrMsg={'There Are No Search Results...'} />
-                    </section>
-                </>
-            )
-        }
-        else {
-            let cardMovies = props.movies.Search.map((elem) => {
-                return <CardMovie key={elem.imdbID}
-                                  Id={elem.imdbID}
-                                  Title={elem.Title}
-                                  Poster={elem.Poster}
-                                  Year={elem.Year} 
-                                  setFlagComponent={setFlagComponent}
-                                  setIdMovie={setIdMovie} />
-            });
+    let cardMovies = props.movies.Search.map((elem) => {
+        return <CardMovie key={elem.imdbID}
+            Id={elem.imdbID}
+            Title={elem.Title}
+            Poster={elem.Poster}
+            Year={elem.Year}
+            setFlagComponent={setFlagComponent}
+            setIdMovie={setIdMovie} />
+    });
 
-            return (
-                <>
-                    <section className={styles.main}>
-                        {cardMovies}
-                    </section>
-
-                </>
-            );
-        }
-    }
+    return (
+        <>
+            <section className={styles.main}>{cardMovies}</section>
+        </>
+    );
 }
+
 
 export default CardList;
